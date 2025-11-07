aa4b45dd7ac52abd41d0c32fb35e8bd069bbf1e7
fabriziom90 <fabriziomastrobattista@gmail.com>
2025-11-07 16:48:58 +0100

    [UPD] Added fontawesome

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <h2>Funtrol</h2>
+          <p>La gestione divertente</p>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../scss/app.scss';
+import '@fortawesome/fontawesome-free/css/all.css'
 
 createApp(App).mount('#app')
